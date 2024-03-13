709fd69c299eac4a2a02ae82b04b12bea14a1046
marina <marina.tharwat1999@gmail.com>
2024-03-13 22:00:59 +0200

    Popular api and design

--- lib/Home/home_page.dart
+++ lib/Home/home_page.dart
@@ -1,11 +1,46 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_app/Api/api_manger.dart';
 import 'package:movies_app/Theme/mytheme.dart';
 
-class HomePage extends StatelessWidget {
+import 'Popular_item.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Map<String, dynamic>> movies = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchMovies();
+  }
+  Future<void> _fetchMovies() async {
+    try {
+      List<Map<String, dynamic>>? fetchedMovies = await ApiManager.getMovies(page: 1);
+      setState(() {
+        movies = fetchedMovies!;
+      });
+    } catch (error) {
+      // Handle error
+      print('Error fetching movies: $error');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      backgroundColor: MyTheme.backgroundColor,
+      appBar: AppBar(backgroundColor: MyTheme.backgroundColor,),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            PopularItems(movies: movies),
+        ],
+        ),
+      ),
     );
   }
 }
